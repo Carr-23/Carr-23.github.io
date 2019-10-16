aa1dab382ab941ed717c8e72e3dc9e80f5ea6546
Cinderguy23 <45177553+Cinderguy23@users.noreply.github.com>
2019-10-16 13:43:46 -0400

    New

--- search.java
+++
@@ -1,7 +0,0 @@
-public int sequentialSearch(Object [ ] arr, Object value){
-    for (int i = 0; i < arr.length ; i++){
-        if (value.equals(arr [i]))
-            return i;
-    }
-    return -1; 
-}
